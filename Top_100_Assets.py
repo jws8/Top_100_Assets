406a06ab4b27df790d1acea6fd5b1ac0e73c3cdc
jws8 <94502439+jws8@users.noreply.github.com>
2021-12-07 03:35:53 -0800

    working

---
+++ Top_100_Assets.py
@@ -0,0 +1,46 @@
+#Top 100 Assets by Market Cap Web Scraper
+#Date: 12/7/2021
+#Author: Joshua W Smith (https://github.com/jws8)
+
+import requests
+from bs4 import BeautifulSoup as BS
+
+url = "https://companiesmarketcap.com/assets-by-market-cap/"
+data = requests.get(url).text
+doc = BS(data, "html.parser")
+tbody = doc.tbody
+trs = tbody.contents
+price_dict = {}
+price_list = []
+
+with open("marketcap.txt", "w") as f:
+    f.write(trs[0].next_sibling.prettify())
+#for tr in trs[1:15]: 
+    #for td in tr:
+        #print(type(td))
+prices = doc.find_all("td", attrs={"class":"td-right"})
+names = doc.find_all("div", attrs={"class": "company-name"})
+
+count = 0
+for j in range(1,len(prices)):
+    if count > 1: 
+        j+=1
+        count = 0
+    else:
+        price = (prices[j].string)
+        price_list.append(price)
+        count+=1
+j = 1
+for i in range(len(names)):
+    #print(str(i+1) + ". " + str(names[i].string))
+    price_dict[names[i].string] = price_list[j].string
+    j+=2
+print("Top 100 Assets by Market Cap: " + "\n" + str(price_dict))
+
+
+
+
+
+
+    
+
