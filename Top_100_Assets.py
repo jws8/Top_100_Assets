5fb099bd044c38d0cc44b891a84518cc984dbb9f
jws8 <94502439+jws8@users.noreply.github.com>
2021-12-07 03:49:17 -0800

    update

--- Top_100_Assets.py
+++ Top_100_Assets.py
@@ -8,13 +8,13 @@ from bs4 import BeautifulSoup as BS
 url = "https://companiesmarketcap.com/assets-by-market-cap/"
 data = requests.get(url).text
 doc = BS(data, "html.parser")
-tbody = doc.tbody
-trs = tbody.contents
+#tbody = doc.tbody
+#trs = tbody.contents
 price_dict = {}
 price_list = []
 
-with open("marketcap.txt", "w") as f:
-    f.write(trs[0].next_sibling.prettify())
+#with open("marketcap.txt", "w") as f:
+    #f.write(trs[0].next_sibling.prettify())
 #for tr in trs[1:15]: 
     #for td in tr:
         #print(type(td))
@@ -22,18 +22,17 @@ prices = doc.find_all("td", attrs={"class":"td-right"})
 names = doc.find_all("div", attrs={"class": "company-name"})
 
 count = 0
-for j in range(1,len(prices)):
+for i in range(1,len(prices)):
     if count > 1: 
-        j+=1
+        i+=1
         count = 0
     else:
-        price = (prices[j].string)
-        price_list.append(price)
+        price_list.append(prices[i].string)
         count+=1
 j = 1
 for i in range(len(names)):
     #print(str(i+1) + ". " + str(names[i].string))
-    price_dict[names[i].string] = price_list[j].string
+    price_dict[names[i].string] = ("#" + str(i+1) + ") " + str(price_list[j-1].string) + ": " + str(price_list[j].string))
     j+=2
 print("Top 100 Assets by Market Cap: " + "\n" + str(price_dict))
 
